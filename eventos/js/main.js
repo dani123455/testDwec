52f844a64e8c89ddc63a6582f758ac21066e444c
Daniel <daniel.mg1705@gmail.com>
2025-01-10 10:52:39 +0100

    10:52

--- eventos/js/main.js
+++ eventos/js/main.js
@@ -1,14 +1,22 @@
 let login = document.getElementById("login");
 let register = document.getElementById("register");
 
+function cambioDeSeccion() {
+    login.addEventListener('click', function() {
+        login.classList.remove("text-secondary");
+        login.classList.add("text-white");
 
-
-function cambioDeSeccion(){
-    login.addEventListener('click', function(){
-        h5.classList.remove("text-secondary");
-        h5.classList.add("text-white");
+        register.classList.remove("text-white");
+        register.classList.add("text-secondary");
     });
 
+    register.addEventListener('click', function() {
+        register.classList.remove("text-secondary");
+        register.classList.add("text-white");
+
+        login.classList.remove("text-white");
+        login.classList.add("text-secondary");
+    });
 }
 
 cambioDeSeccion();
